f5a7eb18053a2f9dfa783007a8128eec7550e5f5
WareForge Inc. <akos9marko@gmail.com>
2025-05-25 18:09:53 +0200

    Updated Vertex

--- vertex.lua
+++ vertex.lua
@@ -1,6 +1,6 @@
 repeat
     local success, err = pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Vertex-MM/Vertex/refs/heads/main/%25loadstring.lua", true))()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring'))()
     end)
     if not success then
         wait(0.5)
