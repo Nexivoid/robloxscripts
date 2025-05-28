9f2a4098234a64b7d8edced99911330b2b168de9
WareForge Inc. <akos9marko@gmail.com>
2025-05-28 15:13:31 +0200

    MiniCrypt Loader

--- minicryptloader.lua
+++ minicryptloader.lua
@@ -9,14 +9,14 @@ if success then
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "MiniCrypt 3.0",
         Text = "Your Load was succesful!",
-        Icon = "rbxassetid://125371290670531",
+        Icon = "125371290670531",
         Duration = 5
     })
 else
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "MiniCrypt 3.0",
         Text = "Your load was unsuccesful! Try again in a few seconds, or restart your executor!",
-        Icon = "rbxassetid://128080248124662",
+        Icon = "128080248124662",
         Duration = 6
     })
 end
