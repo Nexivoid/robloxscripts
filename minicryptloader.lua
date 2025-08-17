4053a8de89330d4f9cd2f468795563d3e6ff07b2
WareForge Inc. <akos9marko@gmail.com>
2025-08-17 17:45:27 +0200

    MiniCrypt 5.0 |-LOADER-|
    Probably the LAST VERSION of MiniCrypt.

--- minicryptloader.lua
+++ minicryptloader.lua
@@ -1,22 +1,22 @@
 wait("0.1")
 
 local success, result = pcall(function()
-	return game:HttpGet("https://raw.githubusercontent.com/WareForgeOfficial/robloxscripts/refs/heads/main/minicrypt4.lua", true)
+	return game:HttpGet("https://raw.githubusercontent.com/WareForgeOfficial/robloxscripts/refs/heads/main/minicrypt5.lua", true)
 end)
 
 if success then
 	loadstring(result)()
 	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = "MiniCrypt 4.0",
+		Title = "MiniCrypt 5.0",
 		Text = "Your Load was succesful!",
-		Icon = "rbxassetid://132142868140148",
+		Icon = "rbxassetid://101848652637352",
 		Duration = 5
 	})
 else
 	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = "MiniCrypt 4.0",
-		Text = "Your load was unsuccesful! Try again in a few seconds, and if it still won't work, that means your executor is not supported!",
-		Icon = "rbxassetid://99549668232787",
+		Title = "MiniCrypt 5.0",
+		Text = "Your load was unsuccesful!",
+		Icon = "rbxassetid://125901806057652",
 		Duration = 6
 	})
 end
