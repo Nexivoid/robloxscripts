0a628a1d28e405a8ea08846d51e335604ea02aa6
WareForge Inc. <akos9marko@gmail.com>
2025-08-17 15:04:08 +0200

    MiniCrypt 4.0 |-LOADER-|
    official

--- minicryptloader.lua
+++ minicryptloader.lua
@@ -1,20 +1,20 @@
 wait("0.1")
 
 local success, result = pcall(function()
-	return game:HttpGet("https://raw.githubusercontent.com/WareForgeOfficial/robloxscripts/refs/heads/main/minicrypt3.lua", true)
+	return game:HttpGet("https://raw.githubusercontent.com/WareForgeOfficial/robloxscripts/refs/heads/main/minicrypt4.lua", true)
 end)
 
 if success then
 	loadstring(result)()
 	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = "MiniCrypt 3.0",
+		Title = "MiniCrypt 4.0",
 		Text = "Your Load was succesful!",
 		Icon = "rbxassetid://132142868140148",
 		Duration = 5
 	})
 else
 	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = "MiniCrypt 3.0",
+		Title = "MiniCrypt 4.0",
 		Text = "Your load was unsuccesful! Try again in a few seconds, or restart your executor!",
 		Icon = "rbxassetid://99549668232787",
 		Duration = 6
