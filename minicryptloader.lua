86b92756165278d036b172cf52e5edb0feeaaaee
Nexivoid <akos9marko@gmail.com>
2025-08-26 19:09:04 +0200

    MiniCrypt Hub 9.2 |-LOADER-|

--- minicryptloader.lua
+++ minicryptloader.lua
@@ -10,7 +10,7 @@ if success then
 		Title = "MiniCrypt Hub 9.2",
 		Text = "Your Load was succesful!",
 		Icon = "\114\98\120\97\115\115\101\116\105\100\58\47\47\49\48\49\56\52\56\54\53\50\54\51\55\51\53\50",
-		Duration = 3.5
+		Duration = 3
 	})
 else
 	game:GetService("StarterGui"):SetCore("SendNotification", {
