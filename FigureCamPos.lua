9f4c441149f55a43f15e640c3b433bac76c56553
Nexivoid <akos9marko@gmail.com>
2025-09-27 17:22:54 +0200

    Delete FigureCamPos.lua

--- FigureCamPos.lua
+++
@@ -1,8 +0,0 @@
-local currentRooms = game.Workspace.CurrentRooms
-local door = currentRooms:FindFirstChild("50")
-local FS = door.FigureSetup
-
-local part = Instance.new("Part")
-part.Name = "FigureCamPos"
-part.Parent = FS
-part.Anchored = true
