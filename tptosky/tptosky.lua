a508ec0f34714a43942cbb83c2c25b4163e4c1db
Nexivoid <akos9marko@gmail.com>
2025-10-12 14:24:29 +0200

    -

---
+++ tptosky/tptosky.lua
@@ -0,0 +1,2 @@
+-- An exploit script that you can teleport to the sky with.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nexivoid/robloxscripts/refs/heads/main/tptosky/loader.luau"))()
